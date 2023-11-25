76f6fb9b08bee2754cc037c927f86397f132a8e0
n4okins <63445867+n4okins@users.noreply.github.com>
2023-11-26 03:15:17 +0900

    Initial Commit

---
+++ Tutorials/LandmarksApp.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksApp.swift
+//  Tutorials
+//
+//  Created by n4okins on 2023/11/26.
+//
+
+import SwiftUI
+
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
