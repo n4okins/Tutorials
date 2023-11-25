cc5daf5522e2ba18204cb2dda85becbc92b4b9d6
n4okins <63445867+n4okins@users.noreply.github.com>
2023-11-26 03:15:17 +0900

    Initial Commit

---
+++ Tutorials/TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialsApp.swift
+//  Tutorials
+//
+//  Created by n4okins on 2023/11/26.
+//
+
+import SwiftUI
+
+@main
+struct TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
